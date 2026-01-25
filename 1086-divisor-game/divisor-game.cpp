3364171ce8b0d500f01c3228152495b1d4bcffd8
Shreya Shukla <shreyashukla218@gmail.com>
2026-01-25 22:06:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (28.71%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,18 +1,26 @@
+// class Solution {
+// public:
+//     int cnt=0;
+//     bool divisorGame(int n) 
+//     {
+//         cnt++;
+//         for (int i=1;i<n;i++)
+//         {
+//         if (n%i==0 && n!=1)
+//         return divisorGame(n-i);
+        
+//         }
+        
+//         if (n==1&&cnt%2==0)
+//         return true;
+//         return false;
+//     }
+// };
 class Solution {
 public:
-    int cnt=0;
+    
     bool divisorGame(int n) 
     {
-        cnt++;
-        for (int i=1;i<n;i++)
-        {
-        if (n%i==0 && n!=1)
-        return divisorGame(n-i);
-        
-        }
-        
-        if (n==1&&cnt%2==0)
-        return true;
-        return false;
+        return n%2==0;
     }
 };
