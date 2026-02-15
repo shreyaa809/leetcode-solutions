935cdd9fc652140ac20e5c46907c8d8d6689612a
Shreya Shukla <shreyashukla218@gmail.com>
2026-02-15 21:53:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (36.83%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    string addBinary(string a, string b) 
+    {
+        int n1=a.length();
+        int n2=b.length();
+        int i=n1-1,j=n2-1;
+        int flag=0;
+        string s="";
+        while (i>=0&&j>=0)
+        {
+            if (a[i]=='0'&&b[j]=='0')
+            {
+            if (flag==0)
+            s+="0";
+            else
+            {
+            s+="1";
+            flag=0;
+            }
+            }
+            else if ((a[i]=='0'&&b[j]=='1')||(a[i]=='1'&&b[j]=='0'))
+            {
+                if (flag==0)
+                s+="1";
+                else
+                s+="0";
+            }
+            else if (a[i]=='1'&&b[j]=='1')
+            {
+                if (flag==1)
+                s+="1";
+                else
+                s+="0";
+                flag=1;
+            }
+            
+            i--;j--;
+        }
+        while (i>=0)
+        {
+            if (a[i]=='0'&&flag==0)
+            s+="0";
+            else if (a[i]=='0'&&flag==1)
+            {
+            s+="1";
+            flag=0;
+            }
+            else if (a[i]=='1'&&flag==0)
+            s+="1";
+            else if (a[i]=='1'&&flag==1)
+            s+="0";
+            i--;
+        }
+        while (j>=0)
+        {
+            if (b[j]=='0'&&flag==0)
+            s+="0";
+            else if (b[j]=='0'&&flag==1)
+            {
+            s+="1";
+            flag=0;
+            }
+            else if (b[j]=='1'&&flag==0)
+            s+="1";
+            else if (b[j]=='1'&&flag==1)
+            s+="0";
+            j--;
+        }
+        if (flag==1)
+        s+="1";
+        reverse(s.begin(),s.end());
+        return s;
+    }
+};
