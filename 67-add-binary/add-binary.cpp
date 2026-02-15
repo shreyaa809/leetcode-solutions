b1fd8aac52e0bc773e62162d96a3402407e767bc
Shreya Shukla <shreyashukla218@gmail.com>
2026-02-15 22:10:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (80.99%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -7,68 +7,88 @@ public:
         int i=n1-1,j=n2-1;
         int flag=0;
         string s="";
-        while (i>=0&&j>=0)
+
+        while (i>=0||j>=0||flag)
         {
-            if (a[i]=='0'&&b[j]=='0')
+            int sum=flag;
+            if (i>=0)
             {
-            if (flag==0)
-            s+="0";
-            else
-            {
-            s+="1";
-            flag=0;
-            }
-            }
-            else if ((a[i]=='0'&&b[j]=='1')||(a[i]=='1'&&b[j]=='0'))
-            {
-                if (flag==0)
-                s+="1";
-                else
-                s+="0";
-            }
-            else if (a[i]=='1'&&b[j]=='1')
-            {
-                if (flag==1)
-                s+="1";
-                else
-                s+="0";
-                flag=1;
-            }
-            
-            i--;j--;
-        }
-        while (i>=0)
-        {
-            if (a[i]=='0'&&flag==0)
-            s+="0";
-            else if (a[i]=='0'&&flag==1)
-            {
-            s+="1";
-            flag=0;
-            }
-            else if (a[i]=='1'&&flag==0)
-            s+="1";
-            else if (a[i]=='1'&&flag==1)
-            s+="0";
+            sum+=a[i]-'0';
             i--;
-        }
-        while (j>=0)
-        {
-            if (b[j]=='0'&&flag==0)
-            s+="0";
-            else if (b[j]=='0'&&flag==1)
-            {
-            s+="1";
-            flag=0;
             }
-            else if (b[j]=='1'&&flag==0)
-            s+="1";
-            else if (b[j]=='1'&&flag==1)
-            s+="0";
-            j--;
+            if (j>=0)
+            {
+                sum+=b[j]-'0';
+                j--;
+            }
+            s+=(sum%2)+'0';
+            flag=sum/2;
+
         }
-        if (flag==1)
-        s+="1";
+
+
+        // while (i>=0&&j>=0)
+        // {
+        //     if (a[i]=='0'&&b[j]=='0')
+        //     {
+        //     if (flag==0)
+        //     s+="0";
+        //     else
+        //     {
+        //     s+="1";
+        //     flag=0;
+        //     }
+        //     }
+        //     else if ((a[i]=='0'&&b[j]=='1')||(a[i]=='1'&&b[j]=='0'))
+        //     {
+        //         if (flag==0)
+        //         s+="1";
+        //         else
+        //         s+="0";
+        //     }
+        //     else if (a[i]=='1'&&b[j]=='1')
+        //     {
+        //         if (flag==1)
+        //         s+="1";
+        //         else
+        //         s+="0";
+        //         flag=1;
+        //     }
+            
+        //     i--;j--;
+        // }
+        // while (i>=0)
+        // {
+        //     if (a[i]=='0'&&flag==0)
+        //     s+="0";
+        //     else if (a[i]=='0'&&flag==1)
+        //     {
+        //     s+="1";
+        //     flag=0;
+        //     }
+        //     else if (a[i]=='1'&&flag==0)
+        //     s+="1";
+        //     else if (a[i]=='1'&&flag==1)
+        //     s+="0";
+        //     i--;
+        // }
+        // while (j>=0)
+        // {
+        //     if (b[j]=='0'&&flag==0)
+        //     s+="0";
+        //     else if (b[j]=='0'&&flag==1)
+        //     {
+        //     s+="1";
+        //     flag=0;
+        //     }
+        //     else if (b[j]=='1'&&flag==0)
+        //     s+="1";
+        //     else if (b[j]=='1'&&flag==1)
+        //     s+="0";
+        //     j--;
+        // }
+        // if (flag==1)
+        // s+="1";
         reverse(s.begin(),s.end());
         return s;
     }
