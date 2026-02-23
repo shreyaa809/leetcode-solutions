69d89b69d26e91bfd112c70a7685a167e2299acd
Shreya Shukla <shreyashukla218@gmail.com>
2026-02-23 11:35:19 +0530

    Time: 157 ms (80.43%) | Memory: 62.1 MB (82.25%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -2,29 +2,20 @@ class Solution {
 public:
     bool hasAllCodes(string s, int k) 
     {
-        set<int>st;
-        int n=s.length();
-        for (int i=0;i<n-k+1;i++)
+        int n = s.length();
+        if (k > n) return false;
+
+        unordered_set<int> seen;
+        int num = 0;
+
+        for (int i = 0; i < n; i++)
         {
-            st.insert(stoi(s.substr(i,k),nullptr,2));
+            num = ((num << 1) & ((1 << k) - 1)) | (s[i] - '0');
+
+            if (i >= k - 1)
+                seen.insert(num);
         }
-        for (int i=0;i<(pow(2,k));i++)
-        {
-            int j=i;
-            string res="";
-            while (j>1)
-            {
-                res+=j%2+'0';
-                j=j/2;
-            }
-            if (j==1) res+='1';
-            reverse(res.begin(),res.end());
-            if (res=="")
-            res+='0';
-            int bin=stoll(res,nullptr,2);
-            if (st.find(bin)==st.end())
-            return false;
-        }
-        return true;
+
+        return seen.size() == (1 << k);
     }
 };
