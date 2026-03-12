8c1edbc99beead1efda6a2c8a9a0e8e5917ef1cb
Shreya Shukla <shreyashukla218@gmail.com>
2026-03-13 01:14:50 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (60.56%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -3,7 +3,14 @@ public:
     int minBitFlips(int start, int goal) 
     {
         int ans=start^goal;
-        int num=__builtin_popcount(ans);
+        int num=0;
+        while (ans>0)
+        {
+            if (ans%2==1)
+            num++;
+            ans/=2;
+        }
+        
         return num;
     }
 };
