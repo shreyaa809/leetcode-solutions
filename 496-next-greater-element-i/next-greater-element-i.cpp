fcc863800583110bd5ec1e7cb1fa71a3c9e492e9
Shreya Shukla <shreyashukla218@gmail.com>
2026-06-03 03:57:12 +0530

    Time: 3 ms (41.06%) | Memory: 12.7 MB (60.70%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
-    vector<int> nextGreater(vector<int>nums2)
+    
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
     {
         int n=nums2.size();
-        int nG=-1;
-        vector<int>ng(n);
+       
+        map<int,int>ng;
         stack<int>st;
         for (int i=n-1;i>=0;i--)
         {
-            if (st.empty())
-            ng[i]=-1;
-            
-            
             while (!st.empty()&&st.top()<=nums2[i])
             st.pop();
             
             if (st.empty())                
-            ng[i]=-1;
+            ng[nums2[i]]=-1;
             else
             {
-            ng[i]=st.top();
+            ng[nums2[i]]=st.top();
             }
             st.push(nums2[i]);
         }
-        return ng;
-    }
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
-    {
-        vector<int>ng=nextGreater(nums2);
         vector<int>ans(nums1.size());
         for (int i=0;i<nums1.size();i++)
         {
-            for(int j=0;j<nums2.size();j++)
-            {
-                if (nums1[i]==nums2[j])
-                ans[i]=ng[j];
-            }
+            ans[i]=ng[nums1[i]];
         }
         return ans;
     }
