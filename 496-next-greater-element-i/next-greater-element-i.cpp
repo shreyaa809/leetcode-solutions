21efbedd131496e9e11601ff091a7d7f67e5a37a
Shreya Shukla <shreyashukla218@gmail.com>
2026-06-03 03:52:47 +0530

    Time: 8 ms (7.37%) | Memory: 12.3 MB (92.01%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -28,7 +28,7 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
     {
         vector<int>ng=nextGreater(nums2);
-        vector<int>ans(nums1);
+        vector<int>ans(nums1.size());
         for (int i=0;i<nums1.size();i++)
         {
             for(int j=0;j<nums2.size();j++)
