3ea0db424e6b6708197469701343e05a2c6c4fdb
Shreya Shukla <shreyashukla218@gmail.com>
2026-07-15 23:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    // int findGcd(int a,int b)
-    // {
-    //     if (a==1||b==1)
-    //     return 1;
-    //     retur
+    int findGcd(int a,int b)
+    {
+        if (b==0)
+        return a;
+        return findGcd(b,a%b);
 
 
-    // }
+    }
     int gcdOfOddEvenSums(int n) 
     {
         long long sumOdd=0,sumEven=0;
         for (int i=1;i<=2*n;i++)
         {
-            if  (n%2!=0)
+            if  (i%2!=0)
             sumOdd+=i;
             else sumEven+=i;
         }
-        // return findGcd(sumOdd,sumEven);
-        return n;
+        return findGcd(sumOdd,sumEven);
+       
     }
 };
