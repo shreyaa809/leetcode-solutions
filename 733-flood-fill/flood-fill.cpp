652c66c11577e5959d2b58edeeca5fd6f6bea3d8
Shreya Shukla <shreyashukla218@gmail.com>
2026-07-24 03:52:24 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (76.37%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,42 +1,20 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& ans, int sr, int sc, int color,int start)
+    {
+        if (sr<0||sc<0||sr>=ans.size()||sc>=ans[0].size()||ans[sr][sc]==color||ans[sr][sc]!=start)
+        return;
+        ans[sr][sc]=color;
+        dfs(ans,sr-1,sc,color,start);
+        dfs(ans,sr+1,sc,color,start);
+        dfs(ans,sr,sc-1,color,start);
+        dfs(ans,sr,sc+1,color,start);
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
     {
-        int startColor=image[sr][sc];
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        int n=image.size();
-        int m=image[0].size();
-        set<pair<int,int>>st;
-        while (!q.empty())
-        {
-            int sizee=q.size();
-            for (int k=0;k<sizee;k++)
-            {
-                int i=q.front().first;
-                int j=q.front().second;
-                q.pop();
-            
-                    image[i][j]=color;
-                    st.insert({i,j});
-                    if (i+1<n &&image[i+1][j]==startColor&&(st.find({i+1,j})==st.end()))
-                    {
-                        q.push({i+1,j});
-                    }
-                    if (i-1>=0 &&image[i-1][j]==startColor &&(st.find({i-1,j})==st.end())  )
-                    {
-                        q.push({i-1,j});
-                    }
-                    if (j+1<m &&image[i][j+1]==startColor &&(st.find({i,j+1})==st.end())  )
-                    {
-                        q.push({i,j+1});
-                    }
-                    if (j-1>=0 &&image[i][j-1]==startColor  &&(st.find({i,j-1})==st.end())  )
-                    {
-                        q.push({i,j-1});
-                    }
-            }
-        }
-        return image;
+        vector<vector<int>>ans=image;
+        int start=image[sr][sc];
+        dfs(ans,sr,sc,color,start);
+        return ans;
     }
 };
