02c02dc38f2282dda4dd7d8ff29d05cb04a83bb5
Shreya Shukla <shreyashukla218@gmail.com>
2026-08-23 15:57:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (74.07%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -11,29 +11,19 @@
  */
 class Solution {
 public:
-    
-    int robber(TreeNode* root,unordered_map<TreeNode*,int>&dp)
+    pair<int,int>robber(TreeNode* root)
     {
         if (root==NULL)
-        return 0;
-        //take
-        if (dp.find(root)!=dp.end())
-        return dp[root];
-        int take = root->val;
-
-        if (root->left)
-        take += robber(root->left->left,dp) + robber(root->left->right,dp);
-
-        if (root->right)
-        take += robber(root->right->left,dp) + robber(root->right->right,dp);
-        //not take
-        int not_take=robber(root->left,dp)+robber(root->right,dp);
-        return dp[root]=max(take,not_take);
-
+        return {0,0};
+        pair<int,int>left=robber(root->left);
+        pair<int,int>right=robber(root->right);
+        int take=root->val+left.second+right.second;
+        int not_take=max(left.first,left.second)+max(right.first,right.second);
+        return {take,not_take};
     }
     int rob(TreeNode* root) 
     {
-        unordered_map<TreeNode*,int>dp;
-        return robber(root,dp);
+        pair<int,int>ans=robber(root);
+        return max(ans.first,ans.second);
     }
 };
