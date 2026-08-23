842c5a7fcbfb314d950a515d167e98a35a2e0fe7
Shreya Shukla <shreyashukla218@gmail.com>
2026-08-23 13:59:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (38.14%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int robber(int idx,vector<int>&nums,vector<int>&dp,int n)
+    int ans=0;
+    int robber(vector<int>& nums,vector<int>& dp,int i,int n)
     {
-        if (idx>=n)
+        if (i>=n)
         return 0;
-        if (dp[idx]!=-1)
-        return dp[idx];
-        int left=nums[idx]+robber(idx+2,nums,dp,n);
-        int right=robber(idx+1,nums,dp,n);
-        return dp[idx]=max(left,right);
-
+        if (dp[i]!=-1)
+        return dp[i];
+        //take
+        dp[i]=nums[i]+robber(nums,dp,i+2,n);
+        //not take
+        return dp[i]=max(dp[i],robber(nums,dp,i+1,n));
     }
     int rob(vector<int>& nums) 
     {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        vector<int>dp1(n+1,-1);
         if (n==1)
         return nums[0];
-        return max(robber(0,nums,dp,n-1),robber(1,nums,dp1,n));
-
+        vector<int>dp1(n,-1);
+        vector<int>dp2(n,-1);
+        int left= robber(nums,dp1,0,n-1);
+        int right=robber(nums,dp2,1,n);
+        return max(left,right);
     }
 };
