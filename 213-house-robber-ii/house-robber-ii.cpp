98b69d18b7981b089bb296240ba7c385349c5a4d
Shreya Shukla <shreyashukla218@gmail.com>
2026-08-23 14:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (99.93%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int ans=0;
-    int robber(vector<int>& nums,vector<int>& dp,int i,int n)
-    {
-        if (i>=n)
-        return 0;
-        if (dp[i]!=-1)
-        return dp[i];
-        //take
-        dp[i]=nums[i]+robber(nums,dp,i+2,n);
-        //not take
-        return dp[i]=max(dp[i],robber(nums,dp,i+1,n));
-    }
+   
     int rob(vector<int>& nums) 
     {
         int n=nums.size();
-        if (n==1)
-        return nums[0];
-        vector<int>dp1(n,-1);
-        vector<int>dp2(n,-1);
-        int left= robber(nums,dp1,0,n-1);
-        int right=robber(nums,dp2,1,n);
-        return max(left,right);
+       if (n==1)
+       return nums[0];
+
+        int prev2=0,prev1=0;
+        for (int i=0;i<n-1;i++)
+        {
+            int curr=max(nums[i]+prev2,prev1);
+            prev2=prev1;
+            prev1=curr;
+        }
+        int prevv1=0,prevv2=0;
+        for (int i=1;i<n;i++)
+        {
+            int curr=max(nums[i]+prevv2,prevv1);
+            prevv2=prevv1;
+            prevv1=curr;
+        }
+        return max(prev1,prevv1);
     }
 };
