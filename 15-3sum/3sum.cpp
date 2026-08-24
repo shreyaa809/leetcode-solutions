7de300be785c94a844fe3e3703f717527204fd30
Shreya Shukla <shreyashukla218@gmail.com>
2026-08-25 05:21:39 +0530

    Time: 51 ms (41.79%) | Memory: 29 MB (92.46%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,34 +3,34 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
         int n=nums.size();
-        set<vector<int>>trip;
+        
         sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
         for (int i=0;i<n-2;i++)
         {
             int j=i+1,k=n-1;
-            while (j<k)
-            {
-                int summ=nums[i]+nums[j]+nums[k];
-                if (summ==0)
-                {
-                trip.insert({nums[i],nums[j],nums[k]});
-                j++;
-                k--;
-                }
-                else if (summ>0)
-                {
-                    while (j<k&&nums[i]+nums[j]+nums[k]>0)
-                    k--;
-                }
-                else if (summ<0)
-                {
-                    while (j<k&&nums[i]+nums[j]+nums[k]<0)
-                    j++;
+            if (nums[i]>0)
+            break;
+            if (i>0&&nums[i]==nums[i-1])
+            continue;
 
-                }
+            while (j<k){
+            int summ=nums[i]+nums[j]+nums[k];
+            if (summ==0)
+            {
+            ans.push_back({nums[i],nums[j],nums[k]});
+            while (j<k&&nums[j]==nums[j+1])
+            j++;
+            while (j<k&&nums[k]==nums[k-1])
+            k--;
+            j++;k--;
             }
+            else if (nums[i]+nums[j]+nums[k]<0)
+            j++;
+            else
+            k--;
         }
-        vector<vector<int>>ans(trip.begin(),trip.end());
+        }
         return ans;
     }
 };
