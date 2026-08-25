5c62ef022c56565a7dc981ea740951be71304457
Shreya Shukla <shreyashukla218@gmail.com>
2026-08-25 17:37:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (80.03%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) 
+    {
+        vector<int>ans(n+1);
+        ans[0]=0;
+        for (int i=1;i<ans.size();i++)
+        {
+            ans[i] = ans[i & (i-1)] + 1;       
+        }
+        return ans;
+    }
+};
