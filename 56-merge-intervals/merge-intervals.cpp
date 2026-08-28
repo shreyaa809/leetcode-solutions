4143aa5d54eafab1d20da69f33ad011021723d02
Shreya Shukla <shreyashukla218@gmail.com>
2026-08-29 00:56:49 +0530

    Time: 3 ms (89.02%) | Memory: 23.9 MB (59.64%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,16 +4,14 @@ public:
     {
         vector<vector<int>>ans;
         int n=nums.size();
-        
         sort(nums.begin(),nums.end());
-        
         for (int i=0;i<n;i++)
         {
             int start=nums[i][0];
             int end=nums[i][1];
             int idx=i+1;
             
-            while (idx<n&&((nums[idx][1]>=end&&nums[idx][0]<=end)||(nums[idx][1]<end&&nums[idx][0]<end)))
+            while (idx<n&&nums[idx][0] <= end)
             { 
                 end=max(nums[idx][1],end);
                 idx++;
